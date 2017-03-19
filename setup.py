7a238f34148f578bf1713a3021f4094549ebb37f
Thomas Holder <thomas.holder@schrodinger.com>
2017-03-19 16:55:38 -0400

    setup.py installation script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="simplemmtf",
+    description='MMTF I/O library',
+    version='0.1',
+    py_modules=[
+        "simplemmtf",
+    ],
+    author='Schrodinger, Inc.',
+    license='BSD-3-Clause',
+)
